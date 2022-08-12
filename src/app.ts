9b0374a1555c428f87ad26f3655b2d7faee6bc33
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-12 05:48:13 -0400

    replaced index.ts with app.ts

---
+++ src/app.ts
@@ -0,0 +1,42 @@
+import express, { Response, Request } from "express"
+// import path from "path"
+// import mongoose from "mongoose"
+// import favicon from "serve-favicon"
+import compression from "compression"
+import helmet from "helmet"
+
+import endpoints from "./endpoints.config"
+// import indexRouter from "./routes"
+// import catalogRouter from "./routes/catalog"
+
+// Connect to MongoDB
+// mongoose.connect(endpoints.MONGO_URL)
+
+const app: express.Express = express()
+
+//Body Parser Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+// view engine setup
+// app.set("views", path.join(__dirname, "..", "views"))
+// app.set("view engine", "pug")
+
+app.use(compression()) // compress all paths
+app.use(helmet()) // protect site by setting appr headers
+
+// serve favico
+// app.use(favicon(path.join(__dirname, "..", "public", "favicon.ico")))
+
+// app.use("/", indexRouter)
+// app.use("/catalog", catalogRouter)
+
+app.get("/", (_: Request, res: Response) => {
+  res.send("Hello World!")
+})
+
+app.listen(endpoints.PORT, () => {
+  console.log(
+    `⚡️[server]: Server is running at https://localhost:${endpoints.PORT}`
+  )
+})
