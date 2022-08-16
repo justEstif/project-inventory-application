d9c73e0c38285fe0893da9c2c3d0d3ebd33bfaba
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-16 13:38:18 -0400

    reordered route to fix error

--- src/routes/inventory.ts
+++ src/routes/inventory.ts
@@ -9,7 +9,6 @@ const inventoryRouter = Router();
 // NOTE: Homepage
 inventoryRouter.get("/", category_controller.index);
 
-
 // GET request for creating a category. NOTE This must come before routes that display category (uses id).
 inventoryRouter.get(
   "/category/create",
@@ -51,14 +50,15 @@ inventoryRouter.post(
 
 /// ITEM ROUTES ///
 
-// GET request for one item.
-inventoryRouter.get("/item/:id", item_controller.item_detail);
 
 // GET request for creating a item. NOTE This must come before routes that display item (uses id).
 inventoryRouter.get("/item/create", item_controller.item_create_get);
 
 // POST request for creating item.
 inventoryRouter.post("/item/create", item_controller.item_create_post);
+
+// GET request for one item.
+inventoryRouter.get("/item/:id", item_controller.item_detail);
 
 // GET request to delete item.
 inventoryRouter.get("/item/:id/delete", item_controller.item_delete_get);
